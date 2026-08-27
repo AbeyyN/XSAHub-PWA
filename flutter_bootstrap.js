eb6b6652c6b8b01259969d7a7bc917c85babcff0
AbeyyTechXy Fujitsu Builder <86111714+AbeyyN@users.noreply.github.com>
2026-08-27 21:15:15 +0800

    deploy: XSAHub PWA 0196418

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1815838153" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3989165500" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
